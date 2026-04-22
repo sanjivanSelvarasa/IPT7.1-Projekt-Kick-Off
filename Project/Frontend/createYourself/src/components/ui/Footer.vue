f06d26be60e5cef216fc38fab3e1f58446dc8e70
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-22 18:00:51 +0200

    Create Footer.vue

---
+++ Project/Frontend/createYourself/src/components/ui/Footer.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="flex items-center justify-between px-6 py-6 bg-[var(--text-color)] text-sm text-[var(--text-color-light)]">
+    <div>
+      <span>© 2026 CreateYourself</span>
+    </div>
+    <div>
+      <span>Entwicklet von Sanjivan, Egor, Gian, Kenan</span>
+    </div>
+  </div>
+</template>
