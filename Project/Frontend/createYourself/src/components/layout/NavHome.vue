3b435ef5b8a797ea09da0caa1f89139d1188c7b4
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-23 17:50:59 +0200

    Update NavHome follows window

--- Project/Frontend/createYourself/src/components/layout/NavHome.vue
+++ Project/Frontend/createYourself/src/components/layout/NavHome.vue
@@ -4,7 +4,7 @@ import Logo from "@/components/ui/Logo.vue";
 </script>
 
 <template>
-  <div class="absolute top-0 left-0 w-full bg-[var(--surface-color)] shadow-sm h-[65px] flex items-center justify-center">
+  <div class="z-999 fixed top-0 left-0 w-full bg-[var(--surface-color)] shadow-sm h-[65px] flex items-center justify-center">
     <div class="flex items-center justify-between w-[1400px] px-4 py-2">
       <!-- Logo-->
       <Logo link="/"></Logo>
