4ae2a4110b45753e8a6d91fe95f64a789f51a53e
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-24 16:11:23 +0200

    Create portfolioType.ts

---
+++ Project/Frontend/createYourself/src/types/portfolioType.ts
@@ -0,0 +1,11 @@
+export type PortfolioType = {
+  id: number,
+  userId: number,
+  templateId: number,
+  title: string,
+  description: string,
+  slug: string,
+  visibility: string,
+  createdAt: Date,
+  updatedAt: Date,
+}
