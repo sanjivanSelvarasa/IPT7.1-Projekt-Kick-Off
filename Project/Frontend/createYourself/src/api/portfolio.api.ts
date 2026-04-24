1e2a405874cfd6a1875c82522e12d567c354f561
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-24 18:15:15 +0200

    Update portfolio.api.ts delete token parameter

--- Project/Frontend/createYourself/src/api/portfolio.api.ts
+++ Project/Frontend/createYourself/src/api/portfolio.api.ts
@@ -2,50 +2,50 @@ import {apiFetch} from "@/api/api.ts";
 import type {CreatePortfolioType} from "@/types/createPortfolioType.ts";
 import type {PortfolioType} from "@/types/portfolioType.ts";
 
-export async function getPortolioApi(token: string){
+export async function getPortolioApi() : Promise<PortfolioType>{
     return await apiFetch(`/portfolio`, {
       method: 'GET',
       headers: {
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${localStorage.getItem('token')}`
       }
     });
 }
 
-export async function getPortfolioByIdApi(id: number, token: string){
+export async function getPortfolioByIdApi(id: number){
   return await apiFetch(`/portfolio/${id}`, {
     method: 'GET',
     headers: {
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${localStorage.getItem('token')}`
     }
   })
 }
 
-export async function createPortfolioApi(portfolio: CreatePortfolioType, token: string){
+export async function createPortfolioApi(portfolio: CreatePortfolioType){
   return await apiFetch(`/portfolio`, {
     method: 'POST',
     body: JSON.stringify({
       portfolio
     }),
     headers: {
-      Authorization: `BEARER ${token}`
+      Authorization: `BEARER ${localStorage.getItem('token')}`
     }
   })
 }
 
-export async function updatePortfolioByIdApi(id: number, portfolio: PortfolioType ,token: string){
+export async function updatePortfolioByIdApi(id: number, portfolio: PortfolioType){
   return await apiFetch(`/portfolio/${id}`, {
     method: 'PUT',
     headers: {
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${localStorage.getItem('token')}`
     }
   })
 }
 
-export async function deletePortfolioApi(id: number, token: string){
+export async function deletePortfolioApi(id: number){
   return await apiFetch(`/portfolio/${id}`, {
     method: 'DELETE',
     headers: {
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${localStorage.getItem('token')}`
     }
   })
 }
