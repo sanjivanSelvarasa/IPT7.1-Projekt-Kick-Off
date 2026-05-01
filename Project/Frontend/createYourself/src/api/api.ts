b82d71d5abfb3561e0b646a77c79ebd27af2b4fe
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-02 00:08:50 +0200

    Update api.ts add refresh token loader

--- Project/Frontend/createYourself/src/api/api.ts
+++ Project/Frontend/createYourself/src/api/api.ts
@@ -1,7 +1,10 @@
 const API_URL = "http://localhost:3000";
+import { useAuthStore } from "@/stores/authStore.ts";
 
 export async function apiFetch<T>(endpoint: string, options?: RequestInit): Promise<T> {
-  const res = await fetch(`${API_URL}${endpoint}`, {
+  const authStore = useAuthStore();
+
+  let res = await fetch(`${API_URL}${endpoint}`, {
     ...options,
     credentials: "include",
     headers: {
@@ -9,6 +12,25 @@ export async function apiFetch<T>(endpoint: string, options?: RequestInit): Prom
       ...(options?.headers || {}),
     }
   });
+
+  if(res.status === 401 || res.status === 403) {
+    try{
+      await authStore.refreshToken();
+
+      res = await fetch(`${API_URL}${endpoint}`, {
+        ...options,
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+          ...(options?.headers || {}),
+          ...(authStore.token ? { Authorization: `BEARER ${authStore.token}` } : {}),
+        }
+      })
+    }catch(err){
+      const text = await res.text();
+      throw new Error(`ERROR: ${res.status}: ${text}`);
+    }
+  }
 
   if(!res.ok){
     const text = await res.text();
