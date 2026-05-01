b89c5e655299524b8cb6805fa4390f671e6ebdb8
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-02 00:09:37 +0200

    Update footer add responsibility

--- Project/Frontend/createYourself/src/components/layout/Footer.vue
+++ Project/Frontend/createYourself/src/components/layout/Footer.vue
@@ -1,14 +1,18 @@
 <script lang="ts" setup>
+  import {useI18n} from "vue-i18n";
 
+  const { t } = useI18n()
+
+  const tl = (key: string) => t(`footer.${key}`)
 </script>
 
 <template>
   <div class="flex items-center justify-between px-6 py-6 bg-[var(--text-color)] text-sm text-[var(--text-color-light)]">
     <div>
-      <span>© 2026 CreateYourself</span>
+      <span class="sm:text-md text-[11px] text-nowrap">{{ tl("Project-name") }}</span>
     </div>
     <div>
-      <span>Entwickelt von Sanjivan, Egor, Gian, Kenan</span>
+      <span class="sm:text-md text-[11px] text-nowrap">{{ tl("created-by") }}</span>
     </div>
   </div>
 </template>
